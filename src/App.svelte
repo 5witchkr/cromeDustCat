900ad7b69580f46ebc71ddb9af6134802f1b2394
5witchkr <hob4419@gmail.com>
2022-05-16 00:01:34 +0900

    feat: setup project

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+
+	
+
+</script>
+
+<main>
+	
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<figure><img src="https://cdn.pixabay.com/photo/2017/11/09/21/41/cat-2934720_960_720.jpg" alt="cat" /></figure>
+		<div class="card-body" style="background-color: skyblue;">
+		  <h2 class="card-title" style="color: black;">오늘의 </h2>
+		  <p style="color: black;">어떻습니다!</p>
+		  <div class="card-actions justify-end">
+			<button class="btn btn-primary">버튼</button>
+		  </div>
+		</div>
+	  </div>
+
+</main>
+
+<style>
+
+</style>
