ad2efa8a3de3ff6f1add94f60ed18232b8002e0c
5witchkr <hob4419@gmail.com>
2022-05-18 00:01:31 +0900

    base frame

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 
+
 	
 
 </script>
@@ -7,15 +8,29 @@
 <main>
 	
 	<div class="card w-96 bg-base-100 shadow-xl">
-		<figure><img src="https://cdn.pixabay.com/photo/2017/11/09/21/41/cat-2934720_960_720.jpg" alt="cat" /></figure>
-		<div class="card-body" style="background-color: skyblue;">
-		  <h2 class="card-title" style="color: black;">오늘의 </h2>
-		  <p style="color: black;">어떻습니다!</p>
-		  <div class="card-actions justify-end">
-			<button class="btn btn-primary">버튼</button>
-		  </div>
-		</div>
-	  </div>
+		<div style="display: flex">
+			<div class="card lg:card-side bg-base-100 shadow-xl">
+				<figure><img src="images/good.png" alt="Album"></figure>
+				<div class="card-body">
+				  <h2 class="card-title">미세먼지 </h2>
+				  <p>좋음</p>
+				  <p>㎍/m³</p>
+				  <div class="card-actions justify-end">
+				  </div>
+				</div>
+			  </div>
+			  <div class="card lg:card-side bg-base-100 shadow-xl">
+				<figure><img src="images/verygood.png" alt="Album"></figure>
+				<div class="card-body">
+				  <h2 class="card-title">초미세먼지</h2>
+				  <p>아주좋음</p>
+				  <p>㎍/m³</p>
+				  <div class="card-actions justify-end">
+				  </div>
+				</div>
+			  </div>
+	  	</div>
+	</div>
 
 </main>
 
